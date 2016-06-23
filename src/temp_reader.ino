4e42b487f33afe3ad33fa8b9d405575d94715b09
tdcrenshaw <tdcrenshaw@gmail.com>
2016-06-23 17:00:06 -0500

    changed samples to arrary
    atom had a hard time pushing so I copied and pasted into browser.
    the for loops with z probably aren't right yet

--- src/temp_reader.ino
+++ src/temp_reader.ino
@@ -2,8 +2,8 @@
 #import esp8266
 
 // which analog pin to connect
-#define GrillTempPin A0
-#define MeatTempPin
+//pins will be multiplexed
+#define TempPin A0
 // resistance at 25 degrees C
 #define THERMISTORNOMINAL 10000
 // temp. for nominal resistance (almost always 25 C)
@@ -16,6 +16,10 @@
 // the value of the 'other' resistor
 #define SERIESRESISTOR 10000
 
+//for now, we'll say one. Change to two when we get another themistor
+//done globably so there's no fuckery with returns and arrarys
+float TempSamples[1];
+
 int samples[NUMSAMPLES];
 
 void setup(void) {
@@ -25,68 +29,65 @@ void setup(void) {
 
 void loop(void) {
   uint8_t i;
-  
-  float GrillAverage;
-  float MeatAverage;
-  float GrillConverted;
-  float MeatConverted;
-  
-  GrillAverage = sample(GrillTempPin);
-  MeatAverage = sample(MeatTempPin);
-  
-  GrillConverted = convert(GrillAverage);
-  MeatConverted = convert(MeatAverage);
-  
+
+  sample(TempPin);
+
+  convert();
+
+
   Serial.println("Grill Temp is: ");
-  Serial.print(GrillConverted);
-  
+  Serial.print(TempSamples[0]);
+
   Serial.println("Meat Temp is: ");
-  Serial.print(MeatConverted);
+  Serial.print(TempSamples[1]);
 
   delay(1000);
 }
 
 void sample(pin) {
-  float average;
-  
-  // take N samples in a row, with a slight delay
-  for (i=0; i< NUMSAMPLES; i++) {
-   samples[i] = analogRead(THERMISTORPIN);
-   delay(10);
-  }
+//figure out how to return them both //used global variables
 
-  // average all the samples out
-  average = 0;
-  for (i=0; i< NUMSAMPLES; i++) {
-     average += samples[i];
-  }
-  average /= NUMSAMPLES;
+    //might could make sizeof work here, but it returns byte size
+    //for now I want it to run once, will run twice eventually
+    for (z=0; z<1; z++) {
+      // take N samples in a row, with a slight delay
+      for (i=0; i< NUMSAMPLES; i++) {
+       samples[i] = analogRead(THERMISTORPIN);
+       delay(10);
+      }
 
-  Serial.print("Average analog reading ");
-  Serial.println(average);
-  return(average) 
+      // average all the samples out
+      TempSamples[z] = 0;
+      for (i=0; i< NUMSAMPLES; i++) {
+         TempSamples[z] += samples[i];
+      }
+      TempSamples[z] /= NUMSAMPLES;
+
+      Serial.print("Average analog reading ");
+      Serial.println(TempSamples[z]);
+
+    }
+
 }
+void convert() {
+    for (z=0; z<1; z++) {
+        // convert the value to resistance
+        TempSamples[z] = 1023 / TempSamples[z] - 1;
+        TempSamples[z] = SERIESRESISTOR / TempSamples[z];
+        Serial.print("Thermistor resistance ");
+        Serial.println(TempSamples[z]);
 
-void convert(average) {
-  
-  // convert the value to resistance
-  average = 1023 / average - 1;
-  average = SERIESRESISTOR / average;
-  Serial.print("Thermistor resistance ");
-  Serial.println(average);
+        float steinhart;
+        steinhart = TempSamples[z] / THERMISTORNOMINAL;     // (R/Ro)
+        steinhart = log(steinhart);                  // ln(R/Ro)
+        steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+        steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+        steinhart = 1.0 / steinhart;                 // Invert
+        steinhart = steinhart * 9 / 5 - 459.67;     // convert to F double check this, I changed it from c to f conversion
 
-  float steinhart;
-  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  steinhart = steinhart * 9 / 5 - 459.67;     // convert to F double check this, I changed it from c to f conversion
-
-  Serial.print("Temperature ");
-  Serial.print(steinhart);
-  Serial.println(" *F");
-  
-  return(stienhart)
-}
+        Serial.print("Temperature ");
+        Serial.print(steinhart);
+        Serial.println(" *F");
+        }
+    }
 }
