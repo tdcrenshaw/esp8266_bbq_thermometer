91d74d22da95a49027cbf7943e87d838fbb01f13
Tyler Crenshaw <tdcrenshaw@gmail.com>
2016-06-26 21:24:12 -0500

    added pushbullet based notifications

--- src/temp_reader.ino
+++ src/temp_reader.ino
@@ -5,6 +5,7 @@
 #include <Wire.h>
 #include "SSD1306.h"
 #include "font.h"
+#include <WiFiClientSecure.h>
 
 //defining display pins
 SSD1306  display(0x3c, D3, D5);
@@ -12,6 +13,10 @@ SSD1306  display(0x3c, D3, D5);
 //WiFi info
 #define WLAN_SSID       "Pretty Fly For a WiFi"
 #define WLAN_PASS       "cubagoodingjr"
+
+//set target temp in F here
+int TargetTemp = 135;
+int TempTrigger = 0;
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -39,7 +44,16 @@ Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, M
 // Setup a feed called 'grill-temp' for publishing changes.
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
 const char GrillTempFeed[] PROGMEM = AIO_USERNAME "/feeds/grill-temp";
-Adafruit_MQTT_Publish GrillTemp = Adafruit_MQTT_Publish(&mqtt, GrillTempFeed);
+Adafruit_MQTT_Publish GrillTempMQTT = Adafruit_MQTT_Publish(&mqtt, GrillTempFeed);
+
+//pushbullet info
+const char* host = "api.pushbullet.com";
+const int httpsPort = 443;
+const char* PushBulletAPIKEY = "o.QTZncBKUF0NpAV3qS1ikq8Ftc83PToHO"; //get it from your pushbullet account
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "2C BC 06 10 0A E0 6E B0 9E 60 E5 96 BA 72 C5 63 93 23 54 B3"; //got it using https://www.grc.com/fingerprints.htm
 
 
 // which analog pin to connect
@@ -59,7 +73,7 @@ Adafruit_MQTT_Publish GrillTemp = Adafruit_MQTT_Publish(&mqtt, GrillTempFeed);
 
 //for now, we'll say one. Change to two when we get another themistor
 //done globably so there's no fuckery with returns and arrarys
-float TempSamples;
+float GrillTemp;
 
 int samples[NUMSAMPLES];
 
@@ -103,20 +117,25 @@ void loop(void) {
 
   convert();
   Serial.print("Grill Temp is: ");
-  Serial.println(TempSamples);
+  Serial.println(GrillTemp);
   Serial.println();
   // Serial.println("Meat Temp is: ");
   // Serial.print(REMOVE);
 
-  mqtt_upload();
+  if (GrillTemp > TargetTemp && TempTrigger == 0){
+      SendNotification();
+      TempTrigger = 1;
+  }
 
   displaytemp();
+
+  mqtt_upload();
 
   delay(1000);
 }
 
 void sample() {
-//figure out how to return them both //used global variables
+    //figure out how to return them both //used global variables
 
     //might could make sizeof work here, but it returns byte size
     //for now I want it to run once, will run twice eventually
@@ -128,68 +147,50 @@ void sample() {
     }
 
     // average all the samples out
-    TempSamples = 0;
+    GrillTemp = 0;
     for (int i=0; i< NUMSAMPLES; i++) {
-     TempSamples += samples[i];
+     GrillTemp += samples[i];
     }
-    TempSamples /= NUMSAMPLES;
+    GrillTemp /= NUMSAMPLES;
 
     Serial.print("Average analog reading: ");
-    Serial.println(TempSamples);
-
-
-
+    Serial.println(GrillTemp);
 }
+
 void convert() {
     // convert the value to resistance
-    TempSamples = 1023 / TempSamples - 1;
-    TempSamples = SERIESRESISTOR / TempSamples;
+    GrillTemp = 1023 / GrillTemp - 1;
+    GrillTemp = SERIESRESISTOR / GrillTemp;
     Serial.print("Thermistor resistance: ");
-    Serial.println(TempSamples);
+    Serial.println(GrillTemp);
 
     float steinhart;
-    steinhart = TempSamples / THERMISTORNOMINAL;     // (R/Ro)
+    steinhart = GrillTemp / THERMISTORNOMINAL;     // (R/Ro)
     steinhart = log(steinhart);                  // ln(R/Ro)
     steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
     steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
     steinhart = 1.0 / steinhart;                 // Invert
-    TempSamples = steinhart * 9 / 5 - 459.67;     // convert to F double check this, I changed it from c to f conversion
+    GrillTemp = steinhart * 9 / 5 - 459.67;     // convert to F double check this, I changed it from c to f conversion
 
     // Serial.print("Temperature ");
-    // Serial.print(TempSamples);
+    // Serial.print(GrillTemp);
     // Serial.println(" *F");
 
     }
 
-
-void mqtt_upload() {
-    // ping adafruit io a few times to make sure we remain connected
-    if(! mqtt.ping(3)) {
-      // reconnect to adafruit io
-      if(! mqtt.connected())
-        connect();
-    }
-
-    if (! GrillTemp.publish((int32_t)TempSamples))
-      Serial.println(F("Grill upload failed."));
-    else
-      Serial.println(F("Grill upload Success!"));
-}
-
-
 void displaytemp() {
 
     //for rounding
-    TempSamples = TempSamples + .5;
+    GrillTemp = GrillTemp + .5;
     // REMOVE = REMOVE + .5;
 
     //convert to int to fit on screen better
-    String GrillString = " Grill: " + String(int(TempSamples));
+    String GrillString = " Grill: " + String(int(GrillTemp));
     // String MeatString = "Meat: " + String(int(REMOVE));
     display.clear();
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.drawString(3,10, GrillString);
-//    display.drawString(0,33, MeatString);
+    //    display.drawString(0,33, MeatString);
     display.display();
 
 }
@@ -223,3 +224,59 @@ void connect() {
     Serial.println(F("Adafruit IO Connected!"));
 
     }
+
+void mqtt_upload() {
+    // ping adafruit io a few times to make sure we remain connected
+    if(! mqtt.ping(3)) {
+      // reconnect to adafruit io
+      if(! mqtt.connected())
+        connect();
+    }
+
+    if (! GrillTempMQTT.publish((int32_t)GrillTemp))
+      Serial.println(F("Grill upload failed."));
+    else
+      Serial.println(F("Grill upload Success!"));
+}
+
+void SendNotification() {
+    // Use WiFiClientSecure class to create TLS connection
+    WiFiClientSecure client;
+    Serial.print("connecting to ");
+    Serial.println(host);
+    if (!client.connect(host, httpsPort)) {
+        Serial.println("connection failed");
+    return;
+    }
+
+    if (client.verify(fingerprint, host)) {
+        Serial.println("certificate matches");
+    } else {
+        Serial.println("certificate doesn't match");
+    }
+    String url = "/v2/pushes";
+    String messagebody = "{\"type\": \"note\", \"title\": \"Meat is Ready!!!\", \"body\": \"The Meat is Currently at the target temp\"}\r\n";
+    Serial.print("requesting URL: ");
+    Serial.println(url);
+
+    client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Authorization: Bearer " + PushBulletAPIKEY + "\r\n" +
+               "Content-Type: application/json\r\n" +
+               "Content-Length: " +
+               String(messagebody.length()) + "\r\n\r\n");
+    client.print(messagebody);
+
+
+
+    Serial.println("request sent");
+
+    //print the response
+
+    while (client.available() == 0);
+
+    while (client.available()) {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+    }
+}
