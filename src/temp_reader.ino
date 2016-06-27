2ec28c5e0ccde239807b3dfd4a17aa354679aebb
Tyler Crenshaw <tdcrenshaw@gmail.com>
2016-06-26 22:45:40 -0500

    added html viewer and pushbullet

--- src/temp_reader.ino
+++ src/temp_reader.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include "SSD1306.h"
 #include "font.h"
+#include <WiFiClientSecure.h>
 
 //defining display pins
 SSD1306  display(0x3c, D3, D5);
@@ -11,10 +12,24 @@ SSD1306  display(0x3c, D3, D5);
 #define WLAN_SSID       "Pretty Fly For a WiFi"
 #define WLAN_PASS       "cubagoodingjr"
 
+//set target temp in F here
+int TargetTemp = 110;
+int TempTrigger = 0;
+
 //sparkfun info
 const char* host = "data.sparkfun.com";
 const char* publicKey = "RM4KV6gqxWcGv5gwvgnb";
 const char* privateKey = "lzNeK5qpEkCeyKrByrvY";
+
+//pushbullet info
+const char* pbhost = "api.pushbullet.com";
+const int httpsPort = 443;
+const char* PushBulletAPIKEY = "o.QTZncBKUF0NpAV3qS1ikq8Ftc83PToHO"; //get it from your pushbullet account
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "2C BC 06 10 0A E0 6E B0 9E 60 E5 96 BA 72 C5 63 93 23 54 B3"; //got it using https://www.grc.com/fingerprints.htm
+
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -36,12 +51,12 @@ WiFiClient client;
 
 //for now, we'll say one. Change to two when we get another themistor
 //done globably so there's no fuckery with returns and arrarys
-float TempSamples;
+float GrillTemp;
 
 int samples[NUMSAMPLES];
 
 void setup(void) {
-  Serial.begin(9600);
+  Serial.begin(115200);
   //analogReference(EXTERNAL);
 
   display.init();
@@ -78,10 +93,15 @@ void loop(void) {
 
   convert();
   Serial.print("Grill Temp is: ");
-  Serial.println(TempSamples);
+  Serial.println(GrillTemp);
   Serial.println();
   // Serial.println("Meat Temp is: ");
-  // Serial.print(TempSamples[1]);
+  // Serial.print(GrillTemp[1]);
+
+  if (GrillTemp > TargetTemp && TempTrigger == 0){
+      SendNotification();
+      TempTrigger = 1;
+  }
 
   displaytemp();
 
@@ -91,7 +111,7 @@ void loop(void) {
 }
 
 void sample() {
-//figure out how to return them both //used global variables
+
 
     //might could make sizeof work here, but it returns byte size
     //for now I want it to run once, will run twice eventually
@@ -102,33 +122,33 @@ void sample() {
     }
 
     // average all the samples out
-    TempSamples = 0;
+    GrillTemp = 0;
     for (int i=0; i< NUMSAMPLES; i++) {
-     TempSamples += samples[i];
+     GrillTemp += samples[i];
     }
-    TempSamples /= NUMSAMPLES;
+    GrillTemp /= NUMSAMPLES;
 
     Serial.print("Average analog reading: ");
-    Serial.println(TempSamples);
+    Serial.println(GrillTemp);
 
 }
 void convert() {
 
-    TempSamples = 1023 / TempSamples - 1;
-    TempSamples = SERIESRESISTOR / TempSamples;
+    GrillTemp = 1023 / GrillTemp - 1;
+    GrillTemp = SERIESRESISTOR / GrillTemp;
     Serial.print("Thermistor resistance: ");
-    Serial.println(TempSamples);
+    Serial.println(GrillTemp);
 
     float steinhart;
-    steinhart = TempSamples / THERMISTORNOMINAL;     // (R/Ro)
+    steinhart = GrillTemp / THERMISTORNOMINAL;     // (R/Ro)
     steinhart = log(steinhart);                  // ln(R/Ro)
     steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
     steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
     steinhart = 1.0 / steinhart;                 // Invert
-    TempSamples = steinhart * 9 / 5 - 459.67;     // convert to F double check this, I changed it from c to f conversion
+    GrillTemp = steinhart * 9 / 5 - 459.67;     // convert to F double check this, I changed it from c to f conversion
 
     // Serial.print("Temperature ");
-    // Serial.print(TempSamples[z]);
+    // Serial.print(GrillTemp[z]);
     // Serial.println(" *F");
 
 
@@ -137,11 +157,11 @@ void convert() {
 void displaytemp() {
 
     //for rounding
-    TempSamples = TempSamples + .5;
+    GrillTemp = GrillTemp + .5;
 
     //convert to int to fit on screen better
-    String GrillString = " Grill: " + String(int(TempSamples));
-    // String MeatString = "Meat: " + String(int(TempSamples[1]));
+    String GrillString = " Grill: " + String(int(GrillTemp));
+    // String MeatString = "Meat: " + String(int(GrillTemp[1]));
     display.clear();
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.drawString(3,10, GrillString);
@@ -156,7 +176,7 @@ void uploaddata() {
     url += "?private_key=";
     url += privateKey;
     url += "&grilltemp=";
-    url += TempSamples;
+    url += GrillTemp;
 
     Serial.print("Requesting URL: ");
     Serial.println(url);
@@ -180,5 +200,47 @@ void uploaddata() {
 
     Serial.println();
     Serial.println("closing connection");
-    delay(600000);
+
+}
+
+void SendNotification() {
+    // Use WiFiClientSecure class to create TLS connection
+    WiFiClientSecure client;
+    Serial.print("connecting to ");
+    Serial.println(host);
+    if (!client.connect(host, httpsPort)) {
+        Serial.println("connection failed");
+    return;
+    }
+
+    if (client.verify(fingerprint, host)) {
+        Serial.println("certificate matches");
+    } else {
+        Serial.println("certificate doesn't match");
+    }
+    String url = "/v2/pushes";
+    String messagebody = "{\"type\": \"note\", \"title\": \"Meat is Ready!!!\", \"body\": \"The Meat is currently at " + String(GrillTemp) + " degrees F\"}\r\n";
+    Serial.print("requesting URL: ");
+    Serial.println(url);
+
+    client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + pbhost + "\r\n" +
+               "Authorization: Bearer " + PushBulletAPIKEY + "\r\n" +
+               "Content-Type: application/json\r\n" +
+               "Content-Length: " +
+               String(messagebody.length()) + "\r\n\r\n");
+    client.print(messagebody);
+
+
+
+    Serial.println("request sent");
+
+    //print the response
+
+    while (client.available() == 0);
+
+    while (client.available()) {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+    }
 }
